67f59a091e53db22abf4ab4d3a558dcc5f18d947
puddinator <42086157+puddinator@users.noreply.github.com>
2018-08-04 18:08:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
-import time
 
+bot = commands.Bot(command_prefix='$')
 
-client = commands.Bot(command_prefix = "$")
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print("Bot is online and connected to Discord")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
-@client.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)             
+bot.run('<YOUR_TOKEN_HERE>')
         
 
 client.run('NDc1MTgyOTY4NDQ5NTMxOTE2.Dkb-MA.skbs9W1IsXkax-xLOrhGh9ed380')
