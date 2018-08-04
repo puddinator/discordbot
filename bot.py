faf2ea6b7c84dbcd761782bbcaabd27050ec6a02
puddinator <42086157+puddinator@users.noreply.github.com>
2018-08-04 17:14:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,59 +1,161 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import requests
+import os
 
-bot = commands.Bot(command_prefix='$')
 
-@bot.event
+type = 1
+client = discord.Client()
+
+players = {}
+
+hendrikid = "227403635166806016"
+
+minutes = 0
+hour = 0
+
+@client.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    print("Eingeloggt als BoredBot V0.1")
+    print(client.user.name)
+    print(client.user.id)
+    print("------------")
+    await client.change_presence(game=discord.Game(name="access with !help"))
 
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
 
-@bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
+@client.event
+async def on_message(message):
+    if message.content.startswith("!test"):
+        await client.send_message(message.channel, "Test erfolgreich")
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
 
-@bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+    if message.content.startswith("!ping"):
+        await client.send_message (message.channel, "PONG!")
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
-    
-    # give info about you here
-    embed.add_field(name="Author", value="<YOUR-USERNAME>")
-    
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
 
-    # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+    if message.content.startswith("!supreme"):
+        await client.send_message (message.channel, "http://www.supremenewyork.com")
 
-    await ctx.send(embed=embed)
 
-bot.remove_command('help')
+    if message.content.startswith("!steam"):
+        await client.send_message (message.channel, "http://steamcommunity.com/id/cautus/")
 
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
 
-    embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
-    embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
-    embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
-    embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
-    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name="$help", value="Gives this message", inline=False)
+    if message.content.startswith("!owner"):
+        await client.send_message(message.channel, "Dieser Bot wurde von Hendrik erstellt. Bin stolz drauf.")
 
-    await ctx.send(embed=embed)
 
-bot.run('NDc1MTgyOTY4NDQ5NTMxOTE2.Dkb-MA.skbs9W1IsXkax-xLOrhGh9ed380')
+    if message.content.startswith("!memes"):
+        await client.send_message(message.channel, "Memes an die Macht!")
+
+
+    if message.content.lower().startswith("!info"):
+        info = discord.Embed(
+            title="Hey, Ich bin der BoredBot :)",
+            color=0xe74c3c,
+            description="Hey, hier siehst Du die aktuellen Commands:\n"
+                        "!Wenn ihn Vorschläge für die Verbesserung des Bots habt, könnt ihr mich gerne anschreiben. Auch im Falle eines Buggs, stehe ich zur Verfügung\n"
+                        "DiscordID: H3ndrik#7385\n"
+                        "\n"
+                        "\n"
+                        "Beta 0.1"
+
+        )
+
+        await client.send_message(message.channel, embed=info)
+
+
+
+    if message.content.startswith("!russia"):
+        response = requests.get("https://i.ytimg.com/vi/d0z_uXA_pdI/maxresdefault.jpg", stream=True)
+        await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename="Bild.png", content="For Mother Russia")
+
+
+    if message.content.lower().startswith("!help"):
+        help = discord.Embed(
+            title="**Hey, Ich bin der BoredBot** :)",
+            color=0xe74c3c,
+            description="hier kannst du alle derzeit möglichen Commands sehen: \n"
+                        "https://pastebin.com/GKcrpTun"
+
+
+
+        )
+        help.set_author(
+            name="*klick hier*",
+            url="https://www.youtube.com/watch?v=MG9e6m_4yVY"
+
+         )
+        help.add_field(
+            name="**Neuerungen bei der V0.2**",
+            value="1. Custom Command bei PN\n" 
+                  "2. Es wurde die Musik Funktion hinzugefügt\n",
+        )
+
+
+
+
+        await client.send_message(message.channel, embed=help)
+
+    if message.content.startswith('!game') and message.author.id == hendrikid:
+        game = message.content[6:]
+        await client.change_presence(game=discord.Game(name=game))
+        await client.send_message(message.channel, "Status zu " + game + " geändert")
+
+    if message.content.startswith("!hardbass"):
+        await client.send_message(message.channel,"Ich heiße Niklas, und das ist mein Hardbass!")
+
+
+
+    if message.content.startswith("!asmr"):
+        await client.send_message(message.channel, "Autonomous Sensory Meridian Response (oft als ASMR abgekürzt) bezeichnet die Erfahrung eines statisch-ähnlichen oder kribbelnden Gefühls auf der Haut, das typischerweise auf der Kopfhaut beginnt und sich am Nacken und der oberen Wirbelsäule entlang bewegt (sogenannte Tingles)")
+
+
+    if message.content.startswith("!gif"):
+        gif_tag = message.content[5:]
+        rgif = g.random(tag=str(gif_tag))
+        response = requests.get(
+            str(rgif.get("data", {}).get('image_original_url')), stream=True
+        )
+        await  client.send_file(message.channel, io.BytesIO(response.raw.read()), filename="video.gif")
+        
+        
+    if message.content.startswith('!uptime'):
+        await client.send_message(message.channel, "**Ich bin schon {0} Stunde/n und {1} Minuten online auf {2}. **".format(hour, minutes, message.server))
+        
+        
+        
+    if message.content.lower().startswith('!flip'): #Coinflip 50/50% chance kopf oder zahl
+        choice = random.randint(1,2)
+        if choice == 1:
+            await client.add_reaction(message, '🌑')
+        if choice == 2:
+            await client.add_reaction(message, '🌕')
+        
+
+async def total_uptime():
+    await client.wait_until_ready()
+    global minutes
+    minutes = 0
+    global hour
+    hour = 0
+    while not client.is_closed:
+        await asyncio.sleep(60)
+        minutes += 1
+        if minutes == 60:
+            minutes = 0
+            hour += 1
+
+client.loop.create_task(total_uptime())    
+
+   
+
+
+
+
+
+
+client.run(str(os.environ.get('NDc1MTgyOTY4NDQ5NTMxOTE2.Dkb-MA.skbs9W1IsXkax-xLOrhGh9ed380')))
